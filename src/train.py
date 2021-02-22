b1cd68560aa01d3948252a5e37ad5183c37e42d9
amitkvikram <amitkvikram@gmail.com>
2021-02-23 00:06:33 +0530

    save plot in train.py

--- src/train.py
+++ src/train.py
@@ -4,6 +4,9 @@ from preprocessing import PreprocessingHelper
 from model import get_model
 from tensorflow.keras.callbacks import ModelCheckpoint
 from tensorflow.keras.layers import Masking
+from scipy.interpolate import make_interp_spline
+from scipy.ndimage.filters import gaussian_filter1d
+import matplotlib.pyplot as plt
 
 preprocessingHelper = PreprocessingHelper()
 train_df = pd.read_csv("./data/train_data.csv", index_col = 0)
@@ -20,7 +23,9 @@ checkpoint = ModelCheckpoint(filepath, monitor='val_acc', verbose=1, save_best_o
 callbacks_list = [checkpoint]
 
 history = lstm_model.fit(X_train, Y_train, validation_data = (X_val, Y_val), 
-            batch_size=64, verbose=1, epochs=200, shuffle=True, callbacks = callbacks_list)
+            batch_size=64, verbose=1, epochs=50, shuffle=True, callbacks = callbacks_list)
+
+# Plot Results
 lstm_model.load_weights("./weights.best.hdf5")
 test_df = pd.read_csv("./data/test_data.csv", index_col = 0)
 test_df, test_df_c = preprocessingHelper.preprocess(test_df)
@@ -36,3 +41,30 @@ def getAcc(X, Y):
     acc = np.sum((Y_t == pred_y)*mask, axis = 0)/np.sum(mask, axis = 0)
 
     return acc
+
+train_acc = getAcc(X_train, Y_train)
+val_acc = getAcc(X_val, Y_val)
+test_acc = getAcc(X_test, Y_test)
+
+x = np.arange(120)
+test_acc_new = gaussian_filter1d(test_acc, sigma=4)
+train_acc_new = gaussian_filter1d(train_acc, sigma=4)
+val_acc_new = gaussian_filter1d(val_acc, sigma=4)
+
+fig, ax = plt.subplots() 
+fig.set_size_inches((10, 5))
+ax.plot(x, train_acc_new, label = 'Training', )
+ax.plot(x, val_acc_new, label = 'Validation',)
+ax.plot(x, test_acc_new, label = 'Test',)
+color = '#000000'
+ax.set_ylabel("Accuracy", fontsize=14, color = color)
+ax.set_xlabel("Balls", fontsize=14, color = color)
+ax.tick_params(axis='x', colors=color)
+ax.tick_params(axis='y', colors=color)
+ax.spines['bottom'].set_color(color)
+ax.spines['top'].set_color(color)
+ax.spines['left'].set_color(color)
+ax.spines['right'].set_color(color)
+ax.set_facecolor((0.0, 0.0, 0.0, 0.0))
+ax.legend(fontsize=14)
+plt.savefig("./img/acc.png")
